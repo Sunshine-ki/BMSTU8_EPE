1050445cd67f1b3a38a4a89678e49fb698868aae
Alis <sukocheva.alis@mail.ru>
2022-04-20 12:23:47 +0300

    сдана

--- lab_07/src/cocomo2.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from c2_mainwindow import *
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = C2MainWindow()
-    sys.exit(app.exec_())
--- lab_07/src/funcpoint.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from fp_mainwindow import *
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = FPMainWindow()
-    sys.exit(app.exec_())
